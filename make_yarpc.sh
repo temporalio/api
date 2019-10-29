a67f618188175007c2fa166d7a762062ca3fc5e5
Alex Shtin <alex@shtin.com>
2019-10-28 17:14:27 -0700

    Remove scripts.

--- make_yarpc.sh
+++
@@ -1,5 +0,0 @@
-# go get -u github.com/gogo/protobuf/protoc-gen-gogoslick
-# go get -u go.uber.org/yarpc/encoding/protobuf/protoc-gen-yarpc-go
-
-protoc --gogoslick_out=paths=source_relative:. *.proto 
-protoc --yarpc-go_out=. *.proto 
